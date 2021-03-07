242f57c938b4fc53220c882d27d54908076cdf86
Hiep Nguyen <thienhiep171193@gmail.com>
2021-03-07 23:09:11 +0700

    Update pagination module for product filter

---
+++ src/modules/pagination/pagination.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="pagination">
+    <div
+      v-if="currentPage !== 0"
+      class="pagination__item"
+      @click="updateCurrentPage(currentPage - 1)"
+    >
+      Prev
+    </div>
+    <div class="pagination__item pagination__item--current">
+      <span v-text="currentPage + 1" />
+      <span>of</span>
+      <span v-text="pageTotal" />
+    </div>
+    <div
+      v-if="currentPage !== (pageTotal - 1)"
+      class="pagination__item"
+      @click="updateCurrentPage(currentPage + 1)"
+    >
+      Next
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pageSize: {
+      type: Number,
+      default: null
+    },
+    currentPage: {
+      type: Number,
+      default: null
+    },
+    pageTotal: {
+      type: Number,
+      default: null
+    }
+  },
+  methods: {
+    updateCurrentPage (page) {
+      this.$emit('update-page', page)
+    }
+  }
+}
+</script>
--- src/modules/product-inner/product-inner.vue
+++ src/modules/product-inner/product-inner.vue
@@ -10,11 +10,19 @@
         <div class="products">
           <div class="grid grid--4">
             <product-item
-              v-for="product in productFiltered"
+              v-for="product in pageItems"
               :key="product.id"
               :product="product"
             />
           </div>
+        </div>
+        <div class="pagination">
+          <pagination
+            :current-page="currentPage"
+            :page-size="pageSize"
+            :page-total="totalPages"
+            @update-page="updateCurrentPage"
+          />
         </div>
       </div>
     </div>
@@ -29,6 +37,13 @@ export default {
       default: () => []
     }
   },
+  data () {
+    return {
+      currentPage: 0,
+      pageSize: 8,
+      pageItems: []
+    }
+  },
   computed: {
     getColors () {
       return this.createFilterByCollection('color', this.productFiltered)
@@ -38,7 +53,13 @@ export default {
     },
     productFiltered () {
       return this.products
+    },
+    totalPages () {
+      return Math.ceil(this.products.length / this.pageSize)
     }
+  },
+  beforeMount () {
+    this.updatePageItems()
   },
   methods: {
     createFilterByCollection (key, products) {
@@ -50,6 +71,18 @@ export default {
         }
       })
       return arr
+    },
+    updateCurrentPage (pageNumber) {
+      console.log(pageNumber)
+      this.currentPage = pageNumber
+      this.updatePageItems()
+    },
+    updatePageItems () {
+      this.pageItems = this.productFiltered.slice(this.currentPage * this.pageSize, (this.currentPage * this.pageSize) + this.pageSize)
+
+      if (this.pageItems.length === 0 && this.currentPage > 0) {
+        this.updateCurrentPage(this.currentPage - 1)
+      }
     }
   }
 }
